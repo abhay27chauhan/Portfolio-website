8a33c88de3e98a0c504b2404939f8b67c0bb18be
Abhay Chauhan <chauhan27.abhay@gmail.com>
2021-01-21 15:08:36 +0530

    Abhay Chauhan Portfolio

---
+++ profile-frontend/src/data/config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  // TODO: change your profile information here
+  name: "Abhay Chauhan",
+  greeting: "Hey 👋",
+  greetingDescription: "I'm Abhay Chauhan and I'm a Full Stack Web Developer and a Competitive Coder!",
+  githubUrl: "https://github.com/abhay27chauhan",
+  linkedinUrl: "https://www.linkedin.com/in/imabhay/",
+  cvLink: "https://drive.google.com/file/d/1SS-bp_F24QQcETvxn7aB72SIgkmsNfWi/view",
+};
